1830022521c9f515aca049b27eb03b46c6f73b0b
Jain Kuriakose <kuriakosejain456@gmail.com>
2024-07-19 23:14:32 +0530

    added map

--- public/js/script.js
+++ public/js/script.js
@@ -16,3 +16,9 @@ if (navigator.geolocation) {
     }
   );
 }
+
+const map = L.map("map").setView([0, 0], 10);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "Realtime tracker",
+}).addTo(map);
