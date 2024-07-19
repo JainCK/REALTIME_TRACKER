7d3b2f522f792ce96e2b04b54d94a79f7bea92b6
Jain Kuriakose <kuriakosejain456@gmail.com>
2024-07-19 23:05:34 +0530

    added geolocation script

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,18 @@
 const scoket = io();
 
-console.log("hey");
+if (navigator.geolocation) {
+  navigator.geolocation.watchPosition(
+    (position) => {
+      const { latitude, longitude } = position.coords;
+      scoket.emit("send-location", { latitude, longitude });
+    },
+    (error) => {
+      console.error(error);
+    },
+    {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    }
+  );
+}
